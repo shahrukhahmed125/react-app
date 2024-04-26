dcb6e2f157fab7a25499a3a9e33698dee9502c1b
Shahrukh Ahmed <shahrukhahmed125@gmail.com>
2024-04-27 02:32:56 +0500

    props

---
+++ src/components/newFile.js
@@ -0,0 +1,12 @@
+import propTypes from 'prop-types';
+import Navbar from "./Navbar";
+
+Navbar.propTypes = {
+    title: propTypes.string.isRequired,
+    aboutText: propTypes.string.isRequired,
+};
+
+Navbar.defaultProps = {
+    title: "Set title here",
+    aboutText: "About text here",
+};
